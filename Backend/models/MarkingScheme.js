698c383054bf95c20771a9aa77c31db504f4ee8f
Dilanka-Nirmal <dilankanirmal138@gmail.com>
2022-05-27 15:55:36 +0530

    Created marking scheme upload function

---
+++ Backend/models/MarkingScheme.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const markingScheme = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    file_path: {
+      type: String,
+      required: true
+    },
+    file_mimetype: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const MarkingScheme = mongoose.model('MarkingScheme', markingScheme);
+
+module.exports = MarkingScheme;
